63be3d9a6816e768ddb60f214877dd471ec07b6b
Esay Etem Saraç <esay35@gmail.com>
2025-08-19 16:45:51 +0300

    Parser, promtpTemplates and chain

---
+++ simplemessage.py
@@ -0,0 +1,28 @@
+from langchain.chains.question_answering.map_reduce_prompt import messages
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage,SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+model = ChatOpenAI( model="gpt-4", temperature=0.1)
+#tempertature 0-1 arası değer alır. 1'e yaklaştıkça alaka oranı düşer.
+
+#messages =[
+#    SystemMessage(content="Translate the following from English to Spanish"),
+#    HumanMessage(content="Hi!"),]
+#Yukarıdaki işlem sadece bir dili çeviriyordu. Aşağıda bunun dinamik halini, birden fazla dile çevrilmişini yapalım.
+
+system_prompt = "Translate the following message into {language}"
+promt_template = ChatPromptTemplate.from_messages([
+    ("system", system_prompt),("user", "{text}")
+])
+
+parser = StrOutputParser()
+
+chain = promt_template | model | parser
+
+if __name__ == '__main__':
+    print(chain.invoke({"language" : "italian", "text" : "Hello World" }))
