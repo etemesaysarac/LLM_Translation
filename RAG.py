83541d6175b74d7193c7a447ba8dda975ca159b9
Esay Etem Saraç <esay35@gmail.com>
2025-08-20 10:31:28 +0300

    Start to write RAG project

---
+++ RAG.py
@@ -0,0 +1,32 @@
+import bs4
+from dotenv import load_dotenv
+from langchain import hub
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI
+from openai import vector_stores
+
+load_dotenv()
+
+llm = ChatOpenAI(model= "gpt-3.5-turbo")
+
+loader = WebBaseLoader(
+    web_path=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    )
+)
+
+docs = loader.load()
+
+
+
+
+if __name__ == "__main__":
+    print(docs)
